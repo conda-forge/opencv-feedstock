f0251aa2c454759675e96efe614d49f4218cba60
Patrick Snape <patricksnape@gmail.com>
2016-05-25 19:46:50 +0100

    First attempt at OpenCV3 build
    Not yet tested on Unix. Should work on Windows.

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,93 +1,76 @@
-@echo off
+@echo ON
+
+git clone https://github.com/Itseez/opencv_contrib --single-branch --branch %PKG_VERSION% --depth 1
+
+rem Patches apply only to opencv_contrib
+git apply --whitespace=fix -p0 "%RECIPE_DIR%\kcftracker.patch"
+git apply --whitespace=fix -p0 "%RECIPE_DIR%\ocr_beamsearch_decoder.patch"
+git apply --whitespace=fix -p0 "%RECIPE_DIR%\ocr_hmm_decoder.patch"
+git apply --whitespace=fix -p0 "%RECIPE_DIR%\dpm.patch"
 
 mkdir build
 cd build
 
-set CMAKE_CONFIG="Release"
-
-:: Set the right msvc version according to Python version
-REM write a temporary batch file to map cl.exe version to visual studio version
-echo @echo 15=9 > msvc_versions.bat
-echo @echo 16=10 >> msvc_versions.bat
-echo @echo 17=11 >> msvc_versions.bat
-echo @echo 18=12 >> msvc_versions.bat
-echo @echo 19=14 >> msvc_versions.bat
-
-REM Run cl.exe to find which version our compiler is
-for /f "delims=" %%A in ('cl /? 2^>^&1 ^| findstr /C:"Version"') do set "CL_TEXT=%%A"
-FOR /F "tokens=1,2 delims==" %%i IN ('msvc_versions.bat') DO echo %CL_TEXT% | findstr /C:"Version %%i" > nul && set VSTRING=%%j && goto FOUND
-EXIT 1
-:FOUND
-
-call :TRIM VSTRING %VSTRING%
-
-set OPENCV_ARCH=x86
-if "%VSTRING%" == "9" (
-    set GENERATOR=Visual Studio 9 2008
-    set OPENCV_VC=vc9
-)
-if "%VSTRING%" == "10" (
-    set GENERATOR=Visual Studio 10 2010
-    set OPENCV_VC=vc10
-)
-if "%VSTRING%" == "14" (
-    set GENERATOR=Visual Studio 14 2015
-    set OPENCV_VC=vc14
-)
-if not defined GENERATOR EXIT 1
-
-if %ARCH% EQU 64 (
-    set GENERATOR=%GENERATOR% Win64
-    set OPENCV_ARCH=x64
+if "%PY3K%" == "0" (
+    echo "Copying stdint.h for windows"
+    copy "%LIBRARY_INC%\stdint.h" %SRC_DIR%\modules\calib3d\include\stdint.h
+    copy "%LIBRARY_INC%\stdint.h" %SRC_DIR%\modules\videoio\include\stdint.h
+    copy "%LIBRARY_INC%\stdint.h" %SRC_DIR%\modules\highgui\include\stdint.h
 )
 
-rem I had to take out the PNG_LIBRARY because it included
-rem a Windows path which caused it to be wrongly escaped
-rem and thus an error. Somehow though, CMAKE still finds
-rem the correct png library...
-cmake .. -G"%GENERATOR%"                            ^
-    -DBUILD_TESTS=0                                 ^
-    -DBUILD_DOCS=0                                  ^
-    -DBUILD_PERF_TESTS=0                            ^
-    -DBUILD_ZLIB=1                                  ^
-    -DBUILD_TIFF=1                                  ^
-    -DBUILD_PNG=1                                   ^
-    -DBUILD_OPENEXR=1                               ^
-    -DBUILD_JASPER=1                                ^
-    -DBUILD_JPEG=1                                  ^
-    -DPYTHON_EXECUTABLE="%PYTHON%"                  ^
-    -DPYTHON_INCLUDE_PATH="%PREFIX%\include"        ^
-    -DPYTHON_LIBRARY="%PREFIX%\libs\python27.lib"   ^
-    -DPYTHON_PACKAGES_PATH="%SP_DIR%"               ^
-    -DWITH_EIGEN=1                                  ^
-    -DWITH_CUDA=0                                   ^
-    -DWITH_OPENNI=0                                 ^
-    -DWITH_FFMPEG=0                                 ^
-    -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%"
+for /F "tokens=1,2 delims=. " %%a in ("%PY_VER%") do (
+   set "PY_MAJOR=%%a"
+   set "PY_MINOR=%%b"
+)
+set PY_LIB=python%PY_MAJOR%%PY_MINOR%.lib
 
-cmake --build . --config %CMAKE_CONFIG% --target ALL_BUILD
-cmake --build . --config %CMAKE_CONFIG% --target INSTALL
-
+cmake .. -LAH -G "NMake Makefiles"                                    ^
+    -DWITH_EIGEN=1                                                    ^
+    -DBUILD_TESTS=0                                                   ^
+    -DBUILD_DOCS=0                                                    ^
+    -DBUILD_PERF_TESTS=0                                              ^
+    -DBUILD_ZLIB=0                                                    ^
+    -DBUILD_opencv_bioinspired=0                                      ^
+    -DBUILD_TIFF=0                                                    ^
+    -DBUILD_PNG=0                                                     ^
+    -DBUILD_OPENEXR=1                                                 ^
+    -DBUILD_JASPER=1                                                  ^
+    -DBUILD_JPEG=0                                                    ^
+    -DWITH_CUDA=0                                                     ^
+    -DWITH_OPENCL=0                                                   ^
+    -DWITH_OPENNI=0                                                   ^
+    -DWITH_FFMPEG=0                                                   ^
+    -DWITH_VTK=0                                                      ^
+    -DINSTALL_C_EXAMPLES=0                                            ^
+    -DOPENCV_EXTRA_MODULES_PATH=%SRC_DIR%\opencv_contrib\modules      ^
+    -DCMAKE_BUILD_TYPE="Release"                                      ^
+    -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX%                           ^
+    -DEXECUTABLE_OUTPUT_PATH=%LIBRARY_BIN%                            ^
+    -DLIBRARY_OUTPUT_PATH=%LIBRARY_BIN%                               ^
+    -DPYTHON%PY_MAJOR%_EXECUTABLE=%PREFIX%\python                     ^
+    -DPYTHON_INCLUDE_DIR=%PREFIX%\include                             ^
+    -DPYTHON_PACKAGES_PATH=%SP_DIR%                                   ^
+    -DPYTHON_LIBRARY=%PREFIX%\libs\%PY_LIB%                           ^
+    -DPYTHON%PY_MAJOR%_NUMPY_INCLUDE_DIRS=%SP_DIR%\numpy\core\include
 if errorlevel 1 exit 1
 
-rem Let's just move the files around to a more sane structure (flat)
-move "%LIBRARY_PREFIX%\%OPENCV_ARCH%\%OPENCV_VC%\bin\*.dll" "%LIBRARY_BIN%"
-move "%LIBRARY_PREFIX%\%OPENCV_ARCH%\%OPENCV_VC%\bin\*.exe" "%LIBRARY_BIN%"
-move "%LIBRARY_PREFIX%\%OPENCV_ARCH%\%OPENCV_VC%\lib\*.lib" "%LIBRARY_LIB%"
-rmdir "%LIBRARY_PREFIX%\%OPENCV_ARCH%" /S /Q
+cmake --build . --target INSTALL --config Release
+if errorlevel 1 exit 1
 
-rem By default cv.py is installed directly in site-packages
-rem Therefore, we have to copy all of the dlls directly into it!
-xcopy "%LIBRARY_BIN%\opencv*.dll" "%SP_DIR%"
+if "%ARCH%" == "64" (
+     robocopy %LIBRARY_PREFIX%\x64\vc%VS_MAJOR%\ %LIBRARY_PREFIX%\ *.* /E
+   ) else (
+     robocopy %LIBRARY_PREFIX%\x86\vc%VS_MAJOR%\ %LIBRARY_PREFIX%\ *.* /E
+)
+if %ERRORLEVEL% GEQ 8 exit 1
 
-goto :eof
+if "%ARCH%" == "32" ( set "OPENCV_ARCH=86")
+if "%ARCH%" == "64" ( set "OPENCV_ARCH=64")
 
-:TRIM
-  SetLocal EnableDelayedExpansion
-  Call :TRIMSUB %%%1%%
-  EndLocal & set %1=%tempvar%
-  GOTO :eof
-
-  :TRIMSUB
-  set tempvar=%*
-  GOTO :eof
+rem Remove files installed in the wrong locations
+rd /S /Q "%LIBRARY_BIN%\Release"
+rd /S /Q "%LIBRARY_PREFIX%\x%OPENCV_ARCH%"
+rem RD is a bit horrible and doesn't return an errorcode properly, so
+rem the errorcode from robocopy is propagated (which is non-zero), so we
+rem forcibly exit 0 here
+exit 0
--- recipe/build.sh
+++ recipe/build.sh
@@ -1,40 +1,58 @@
 #!/bin/bash
 
+SHORT_OS_STR=$(uname -s)
+
+if [ "${SHORT_OS_STR:0:5}" == "Linux" ]; then
+    DYNAMIC_EXT="so"
+    OPENMP="-DWITH_OPENMP=1"
+    IS_OSX=0
+    # There's a bug with CMake at the moment whereby it can't download
+    # using HTTPS - so we use curl to download the IPP library
+    # mkdir -p $SRC_DIR/3rdparty/ippicv/downloads/linux-808b791a6eac9ed78d32a7666804320e
+    # curl -L https://raw.githubusercontent.com/Itseez/opencv_3rdparty/81a676001ca8075ada498583e4166079e5744668/ippicv/ippicv_linux_20151201.tgz -o $SRC_DIR/3rdparty/ippicv/downloads/linux-808b791a6eac9ed78d32a7666804320e/ippicv_linux_20151201.tgz
+fi
+if [ "${SHORT_OS_STR}" == "Darwin" ]; then
+    DYNAMIC_EXT="dylib"
+    OPENMP=""
+
+    # Require C++11
+    export MACOSX_DEPLOYMENT_TARGET="10.7"
+fi
+
+if [ $PY3K -eq 1 ]; then
+    PY_VER_M="${PY_VER}m"
+    OCV_PYTHON="-DBUILD_opencv_python3=1 -DPYTHON3_EXECUTABLE=$PYTHON -DPYTHON3_INCLUDE_DIR=$PREFIX/include/python${PY_VER_M} -DPYTHON3_LIBRARY=${PREFIX}/lib/libpython${PY_VER_M}.${DYNAMIC_EXT}"
+else
+    OCV_PYTHON="-DBUILD_opencv_python2=1 -DPYTHON2_EXECUTABLE=$PYTHON -DPYTHON2_INCLUDE_DIR=$PREFIX/include/python${PY_VER} -DPYTHON2_LIBRARY=${PREFIX}/lib/libpython${PY_VER}.${DYNAMIC_EXT} -DPYTHON_INCLUDE_DIR2=$PREFIX/include/python${PY_VER}"
+fi
+
+git clone https://github.com/Itseez/opencv_contrib --single-branch --branch $PKG_VERSION --depth 1
+
 mkdir build
 cd build
 
-if [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-  DYNAMIC_EXT="so"
-  OPENMP="-DWITH_OPENMP=1"
-fi
-if [ "$(uname -s)" == "Darwin" ]; then
-  DYNAMIC_EXT="dylib"
-  OPENMP=""
-fi
-
-cmake -LAH ..                                                            \
-    $OPENMP                                                              \
-    -DCMAKE_SKIP_RPATH=1                                                 \
-    -DWITH_EIGEN=1                                                       \
-    -DBUILD_opencv_apps=0                                                \
-    -DBUILD_TESTS=0                                                      \
-    -DBUILD_DOCS=0                                                       \
-    -DBUILD_PERF_TESTS=0                                                 \
-    -DBUILD_ZLIB=0                                                       \
-    -DZLIB_LIBRARY=$PREFIX/lib/libz.$DYNAMIC_EXT                         \
-    -DBUILD_TIFF=0                                                       \
-    -DBUILD_PNG=0                                                        \
-    -DBUILD_OPENEXR=1                                                    \
-    -DBUILD_JASPER=0                                                     \
-    -DBUILD_JPEG=0                                                       \
-    -DPYTHON_EXECUTABLE=$PREFIX/bin/python${PY_VER}                      \
-    -DPYTHON_INCLUDE_PATH=$PREFIX/include/python${PY_VER}                \
-    -DPYTHON_LIBRARY=$PREFIX/lib/libpython${PY_VER}.$DYNAMIC_EXT         \
-    -DPYTHON_PACKAGES_PATH=$SP_DIR                                       \
-    -DWITH_CUDA=0                                                        \
-    -DWITH_OPENCL=0                                                      \
-    -DWITH_OPENNI=0                                                      \
-    -DWITH_FFMPEG=0                                                      \
+cmake ..                                                  \
+    $OPENMP                                               \
+    $OCV_PYTHON                                           \
+    -DWITH_EIGEN=1                                        \
+    -DBUILD_TESTS=0                                       \
+    -DBUILD_DOCS=0                                        \
+    -DBUILD_PERF_TESTS=0                                  \
+    -DBUILD_ZLIB=0                                        \
+    -DBUILD_TIFF=0                                        \
+    -DBUILD_PNG=0                                         \
+    -DBUILD_OPENEXR=1                                     \
+    -DBUILD_JASPER=0                                      \
+    -DBUILD_JPEG=0                                        \
+    -DWITH_CUDA=0                                         \
+    -DWITH_OPENCL=0                                       \
+    -DWITH_OPENNI=0                                       \
+    -DWITH_FFMPEG=0                                       \
+    -DWITH_VTK=0                                          \
+    -DINSTALL_C_EXAMPLES=0                                \
+    -DOPENCV_EXTRA_MODULES_PATH="opencv_contrib/modules"  \
+    -DCMAKE_BUILD_TYPE="Release"                          \
+    -DCMAKE_SKIP_RPATH:bool=ON                            \
     -DCMAKE_INSTALL_PREFIX=$PREFIX
 
 make
