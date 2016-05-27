c855ad3152a03ed220271a7dd82c24a80d9922f0
Patrick Snape <patrick.snape08@imperial.ac.uk>
2016-05-27 09:53:22 +0100

    Reinstate CMAKE IPP hack
    This should probably be checked out actually. Nevertheless, we can still [skip appveyor].

--- recipe/build.sh
+++ recipe/build.sh
@@ -8,8 +8,8 @@ if [ "${SHORT_OS_STR:0:5}" == "Linux" ]; then
     IS_OSX=0
     # There's a bug with CMake at the moment whereby it can't download
     # using HTTPS - so we use curl to download the IPP library
-    # mkdir -p $SRC_DIR/3rdparty/ippicv/downloads/linux-808b791a6eac9ed78d32a7666804320e
-    # curl -L https://raw.githubusercontent.com/Itseez/opencv_3rdparty/81a676001ca8075ada498583e4166079e5744668/ippicv/ippicv_linux_20151201.tgz -o $SRC_DIR/3rdparty/ippicv/downloads/linux-808b791a6eac9ed78d32a7666804320e/ippicv_linux_20151201.tgz
+    mkdir -p $SRC_DIR/3rdparty/ippicv/downloads/linux-808b791a6eac9ed78d32a7666804320e
+    curl -L https://raw.githubusercontent.com/Itseez/opencv_3rdparty/81a676001ca8075ada498583e4166079e5744668/ippicv/ippicv_linux_20151201.tgz -o $SRC_DIR/3rdparty/ippicv/downloads/linux-808b791a6eac9ed78d32a7666804320e/ippicv_linux_20151201.tgz
 fi
 if [ "${SHORT_OS_STR}" == "Darwin" ]; then
     DYNAMIC_EXT="dylib"
