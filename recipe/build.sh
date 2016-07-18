c75853e890a0282c44fb6e027d2fcd893a08a64b
John Kirkham <kirkhamj@janelia.hhmi.org>
2016-07-18 18:44:14 -0400

    recipe: Use the `toolchain`.

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+
+# FIXME: This is a hack to make sure the environment is activated.
+# The reason this is required is due to the conda-build issue
+# mentioned below.
+#
+# https://github.com/conda/conda-build/issues/910
+#
+source activate "${CONDA_DEFAULT_ENV}"
+
 mkdir build
 cd build
 
