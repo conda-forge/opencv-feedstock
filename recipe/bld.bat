dbd827e3b2cd6aad05366920900b9e7e80e165d9
Patrick Snape <patricksnape@gmail.com>
2017-01-23 23:19:56 +0100

    Should build on OSX and Linux
    Had to move Python detection on Unix back into the first CMake
    call because the cache no longer seems to update

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -9,16 +9,6 @@ if NOT "%CONTRIB_SHA256%" == "ef2084bcd4c3812eb53c21fa81477d800e8ce8075b68d9dede
     exit 1
 )
 
-rem Patches apply only to opencv_contrib so we have to apply them now (after source download above)
-rem Fixed: https://github.com/opencv/opencv_contrib/blob/6cd8e9f556c8c55c05178dec05d5277ae00020d9/modules/tracking/src/trackerKCF.cpp#L669
-git apply --whitespace=fix -p0 "%RECIPE_DIR%\kcftracker.patch"
-rem Fixed: https://github.com/opencv/opencv_contrib/blob/master/modules/text/src/ocr_beamsearch_decoder.cpp#L569
-git apply --whitespace=fix -p0 "%RECIPE_DIR%\ocr_beamsearch_decoder.patch"
-rem Fixed: https://github.com/opencv/opencv_contrib/blob/master/modules/text/src/ocr_hmm_decoder.cpp#L985
-git apply --whitespace=fix -p0 "%RECIPE_DIR%\ocr_hmm_decoder.patch"
-rem Fixed: https://github.com/opencv/opencv_contrib/blob/master/modules/dpm/src/dpm_nms.cpp#L43
-git apply --whitespace=fix -p0 "%RECIPE_DIR%\dpm.patch"
-
 mkdir build
 cd build
 
--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,4 @@
-#!/bin/bash
-
+set +x
 SHORT_OS_STR=$(uname -s)
 
 if [ "${SHORT_OS_STR:0:5}" == "Linux" ]; then
@@ -9,14 +8,31 @@ if [ "${SHORT_OS_STR}" == "Darwin" ]; then
     OPENMP=""
 fi
 
-INC_PYTHON="${PREFIX}/include/python${PY_VER}"
-
 curl -L -O "https://github.com/opencv/opencv_contrib/archive/$PKG_VERSION.tar.gz"
-test `openssl sha256 $PKG_VERSION.tar.gz | awk '{print $2}'` = "ef2084bcd4c3812eb53c21fa81477d800e8ce8075b68d9dedec90fef395156e5"
+test `openssl sha256 $PKG_VERSION.tar.gz | awk '{print $2}'` = "1e2bb6c9a41c602904cc7df3f8fb8f98363a88ea564f2a087240483426bf8cbe"
 tar -zxf $PKG_VERSION.tar.gz
+
+# Contrib has patches that need to be applied
+# https://github.com/opencv/opencv_contrib/issues/919
+git apply -p0 $RECIPE_DIR/opencv_contrib_freetype.patch
 
 mkdir build
 cd build
+
+if [ $PY3K -eq 1 ]; then
+    PY_MAJOR=3
+    PY_UNSET_MAJOR=2
+    LIB_PYTHON="${PREFIX}/lib/libpython${PY_VER}m${SHLIB_EXT}"
+    INC_PYTHON="$PREFIX/include/python${PY_VER}m"
+else
+    PY_MAJOR=2
+    PY_UNSET_MAJOR=3
+    LIB_PYTHON="${PREFIX}/lib/libpython${PY_VER}${SHLIB_EXT}"
+    INC_PYTHON="$PREFIX/include/python${PY_VER}"
+fi
+
+PYTHON_SETTINGS="-DPYTHON_EXECUTABLE=${PYTHON} -DPYTHON_INCLUDE_DIR=${INC_PYTHON} -DPYTHON_LIBRARY=${LIB_PYTHON} -DPYTHON_PACKAGES_PATH=${SP_DIR} -DBUILD_opencv_python${PY_MAJOR}=1 -DPYTHON${PY_MAJOR}_EXECUTABLE=${PYTHON} -DPYTHON${PY_MAJOR}_INCLUDE_DIR=${INC_PYTHON} -DPYTHON${PY_MAJOR}_NUMPY_INCLUDE_DIRS=${SP_DIR}/numpy/core/include -DPYTHON${PY_MAJOR}_LIBRARY=${LIB_PYTHON} -DPYTHON${PY_MAJOR}_PACKAGES_PATH=${SP_DIR}"
+PYTHON_UNSETTINGS="-DBUILD_opencv_python${PY_UNSET_MAJOR}=0 -DPYTHON${PY_UNSET_MAJOR}_EXECUTABLE= -DPYTHON${PY_UNSET_MAJOR}_INCLUDE_DIR= -DPYTHON${PY_UNSET_MAJOR}_NUMPY_INCLUDE_DIRS= -DPYTHON${PY_UNSET_MAJOR}_LIBRARY= -DPYTHON${PY_UNSET_MAJOR}_PACKAGES_PATH="
 
 # For some reason OpenCV just won't see hdf5.h without updating the CFLAGS
 export CFLAGS="$CFLAGS -I$PREFIX/include"
@@ -24,14 +40,34 @@ export CXXFLAGS="$CXXFLAGS -I$PREFIX/include"
 
 cmake .. -LAH                                                             \
     $OPENMP                                                               \
+    -DOpenBLAS=1                                                          \
+    -DOpenBLAS_INCLUDE_DIR=$PREFIX/include                                \
+    -DOpenBLAS_LIB=$PREFIX/lib/libopenblas$SHLIB_EXT                      \
     -DWITH_EIGEN=1                                                        \
     -DBUILD_TESTS=0                                                       \
     -DBUILD_DOCS=0                                                        \
     -DBUILD_PERF_TESTS=0                                                  \
     -DBUILD_ZLIB=0                                                        \
-    -DZLIB_LIBRARY_RELEASE=$PREFIX/lib/libz${SHLIB_EXT}                   \
+    -DHDF5_DIR=$PREFIX                                                    \
+    -DHDF5_INCLUDE_DIRS=$PREFIX/include                                   \
+    -DHDF5_C_LIBRARY_hdf5=$PREFIX/lib/libhdf5$SHLIB_EXT                   \
+    -DHDF5_C_LIBRARY_z=$PREFIX/lib/libz$SHLIB_EXT                         \
+    -DFREETYPE_INCLUDE_DIRS=$PREFIX/include/freetype2                     \
+    -DFREETYPE_LIBRARIES=$PREFIX/lib/libfreetype$SHLIB_EXT                \
+    -DPNG_LIBRARY_RELEASE=$PREFIX/lib/libpng$SHLIB_EXT                    \
+    -DPNG_INCLUDE_DIRS=$PREFIX/include                                    \
+    -DJPEG_INCLUDE_DIR=$PREFIX/include                                    \
+    -DJPEG_LIBRARY=$PREFIX/lib/libjpeg$SHLIB_EXT                          \
+    -DTIFF_INCLUDE_DIR=$PREFIX/include                                    \
+    -DTIFF_LIBRARY=$PREFIX/lib/libtiff$SHLIB_EXT                          \
+    -DJASPER_INCLUDE_DIR=$PREFIX/include                                  \
+    -DJASPER_LIBRARY_RELEASE=$PREFIX/lib/libjasper$SHLIB_EXT              \
+    -DWEBP_INCLUDE_DIR=$PREFIX/include                                    \
+    -DWEBP_LIBRARY=$PREFIX/lib/libwebp$SHLIB_EXT                          \
+    -DHARFBUZZ_LIBRARIES=$PREFIX/lib/libharfbuzz$SHLIB_EXT                \
+    -DZLIB_LIBRARY_RELEASE=$PREFIX/lib/libz$SHLIB_EXT                     \
     -DZLIB_INCLUDE_DIR=$PREFIX/include                                    \
-    -DHDF5_z_LIBRARY_RELEASE=$PREFIX/lib/libz${SHLIB_EXT}                 \
+    -DHDF5_z_LIBRARY_RELEASE=$PREFIX/lib/libz$SHLIB_EXT                   \
     -DBUILD_TIFF=0                                                        \
     -DBUILD_PNG=0                                                         \
     -DBUILD_OPENEXR=1                                                     \
@@ -43,48 +79,14 @@ cmake .. -LAH                                                             \
     -DWITH_FFMPEG=0                                                       \
     -DWITH_MATLAB=0                                                       \
     -DWITH_VTK=0                                                          \
+    -DWITH_GPHOTO2=0                                                      \
     -DINSTALL_C_EXAMPLES=0                                                \
+     $PYTHON_SETTINGS                                                     \
+     $PYTHON_UNSETTINGS                                                   \
     -DOPENCV_EXTRA_MODULES_PATH="../opencv_contrib-$PKG_VERSION/modules"  \
     -DCMAKE_BUILD_TYPE="Release"                                          \
     -DCMAKE_SKIP_RPATH:bool=ON                                            \
-    -DCMAKE_INSTALL_PREFIX=$PREFIX                                        \
-    -DBUILD_opencv_python2=0                                              \
-    -DPYTHON2_EXECUTABLE=""                                               \
-    -DPYTHON2_INCLUDE_DIR=""                                              \
-    -DPYTHON2_NUMPY_INCLUDE_DIRS=""                                       \
-    -DPYTHON2_LIBRARY=""                                                  \
-    -DPYTHON_INCLUDE_DIR2=""                                              \
-    -DPYTHON2_PACKAGES_PATH=""                                            \
-    -DBUILD_opencv_python3=0                                              \
-    -DPYTHON3_EXECUTABLE=""                                               \
-    -DPYTHON3_NUMPY_INCLUDE_DIRS=""                                       \
-    -DPYTHON3_INCLUDE_DIR=""                                              \
-    -DPYTHON3_LIBRARY=""                                                  \
-    -DPYTHON3_PACKAGES_PATH=""
-
-
-IFS='.' read -ra PY_VER_ARR <<< "${PY_VER}"
-PY_MAJOR="${PY_VER_ARR[0]}"
-
-if [ $PY3K -eq 1 ]; then
-    LIB_PYTHON="${PREFIX}/lib/libpython${PY_VER}m${SHLIB_EXT}"
-    INC_PYTHON="$PREFIX/include/python${PY_VER}m"
-else
-    LIB_PYTHON="${PREFIX}/lib/libpython${PY_VER}${SHLIB_EXT}"
-    INC_PYTHON="$PREFIX/include/python${PY_VER}"
-fi
-
-cmake .. -LAH                                                                     \
-    -DPYTHON_EXECUTABLE="${PYTHON}"                                               \
-    -DPYTHON_INCLUDE_DIR="${INC_PYTHON}"                                          \
-    -DPYTHON_LIBRARY="${LIB_PYTHON}"                                              \
-    -DPYTHON_PACKAGES_PATH="${SP_DIR}"                                            \
-    -DBUILD_opencv_python${PY_MAJOR}=1                                            \
-    -DPYTHON${PY_MAJOR}_EXECUTABLE=${PYTHON}                                      \
-    -DPYTHON${PY_MAJOR}_INCLUDE_DIR=${INC_PYTHON}                                 \
-    -DPYTHON${PY_MAJOR}_NUMPY_INCLUDE_DIRS=${SP_DIR}/numpy/core/include           \
-    -DPYTHON${PY_MAJOR}_LIBRARY=${LIB_PYTHON}                                     \
-    -DPYTHON${PY_MAJOR}_PACKAGES_PATH=${SP_DIR}
+    -DCMAKE_INSTALL_PREFIX=$PREFIX
 
 make -j8
 make install
