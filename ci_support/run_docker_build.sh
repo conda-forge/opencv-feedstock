78b2a8e6fcd199369a8b0776ca9739dee5943f35
John Kirkham <kirkhamj@janelia.hhmi.org>
2017-06-11 18:09:21 -0400

    MNT: Re-rendered with conda-smithy 2.3.1
    Re-render to add NumPy 1.13 to the CI matrices.

--- ci_support/run_docker_build.sh
+++ ci_support/run_docker_build.sh
@@ -57,7 +57,7 @@ conda clean --lock
 conda install --yes --quiet conda-forge-build-setup
 source run_conda_forge_build_setup
 
-# Embarking on 6 case(s).
+# Embarking on 9 case(s).
     set -x
     export CONDA_NPY=111
     export CONDA_PY=27
@@ -67,6 +67,13 @@ source run_conda_forge_build_setup
 
     set -x
     export CONDA_NPY=112
+    export CONDA_PY=27
+    set +x
+    conda build /recipe_root --quiet || exit 1
+    upload_or_check_non_existence /recipe_root conda-forge --channel=main || exit 1
+
+    set -x
+    export CONDA_NPY=113
     export CONDA_PY=27
     set +x
     conda build /recipe_root --quiet || exit 1
@@ -81,6 +88,13 @@ source run_conda_forge_build_setup
 
     set -x
     export CONDA_NPY=112
+    export CONDA_PY=35
+    set +x
+    conda build /recipe_root --quiet || exit 1
+    upload_or_check_non_existence /recipe_root conda-forge --channel=main || exit 1
+
+    set -x
+    export CONDA_NPY=113
     export CONDA_PY=35
     set +x
     conda build /recipe_root --quiet || exit 1
@@ -95,6 +109,13 @@ source run_conda_forge_build_setup
 
     set -x
     export CONDA_NPY=112
+    export CONDA_PY=36
+    set +x
+    conda build /recipe_root --quiet || exit 1
+    upload_or_check_non_existence /recipe_root conda-forge --channel=main || exit 1
+
+    set -x
+    export CONDA_NPY=113
     export CONDA_PY=36
     set +x
     conda build /recipe_root --quiet || exit 1
