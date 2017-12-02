5fdf78c345228020e2c0615903c7f1ff06bea3fd
Patrick Snape <patricksnape@gmail.com>
2017-12-02 21:17:40 +0100

    Add certifi to try getting around certificate error

--- recipe/run_test.py
+++ recipe/run_test.py
@@ -3,11 +3,8 @@ import tempfile
 import os.path as op
 import platform
 import shutil
+import requests
 import cv2
-try:
-    from urllib.request import urlretrieve
-except ImportError:
-    from urllib import urlretrieve
 
 
 OPENCV_AVI_URL = 'https://github.com/opencv/opencv_extra/raw/master/testdata/highgui/video/VID00003-20100701-2204.avi'
@@ -21,7 +18,9 @@ class TestVideoRead(unittest.TestCase):
     def setUpClass(cls):
         cls.temp_dir = tempfile.mkdtemp()
         cls.avi_path = op.join(cls.temp_dir, 'test.avi')
-        urlretrieve(OPENCV_AVI_URL, cls.avi_path)
+        req = requests.get(OPENCV_AVI_URL, stream=True)
+        with open(cls.avi_path, 'wb') as f:
+            shutil.copyfileobj(req.raw, f)
 
     @classmethod
     def tearDownClass(cls):
