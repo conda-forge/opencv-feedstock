2ea1be7c2e4a6c242ee5572c051ec937f4a5c6da
John Kirkham <kirkhamj@janelia.hhmi.org>
2018-03-09 18:45:02 -0500

    Update opencv_contrib checksum
    As we are on a new version, update the opencv_contrib checksum.

--- recipe/build.sh
+++ recipe/build.sh
@@ -16,7 +16,7 @@ if [ "${SHORT_OS_STR}" == "Darwin" ]; then
 fi
 
 curl -L -O "https://github.com/opencv/opencv_contrib/archive/$PKG_VERSION.tar.gz"
-test `openssl sha256 $PKG_VERSION.tar.gz | awk '{print $2}'` = "e94acf39cd4854c3ef905e06516e5f74f26dddfa6477af89558fb40a57aeb444"
+test `openssl sha256 $PKG_VERSION.tar.gz | awk '{print $2}'` = "298c69ee006d7675e1ff9d371ba8b0d9e7e88374bb7ba0f9d0789851d352ec6e"
 tar -zxf $PKG_VERSION.tar.gz
 
 mkdir -p build
