8857a16478c4b28c09d8129cb2fe9cc6dbdeafb8
Mark Harfouche <mark.harfouche@gmail.com>
2019-11-11 00:52:51 -0500

    Simplify tests by using conda-build to get files

--- recipe/run_py_test.py
+++ recipe/run_py_test.py
@@ -9,24 +9,13 @@ import requests
 
 import cv2
 
-OPENCV_AVI_URL = 'https://github.com/opencv/opencv_extra/raw/master/testdata/highgui/video/VID00003-20100701-2204.avi'
-
-
 @unittest.skipIf(platform.system() == 'Windows',
                  'FFMPEG currently not built on Windows')
 class TestVideoRead(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        cls.temp_dir = tempfile.mkdtemp()
-        cls.avi_path = op.join(cls.temp_dir, 'test.avi')
-        req = requests.get(OPENCV_AVI_URL, stream=True)
-        with open(cls.avi_path, 'wb') as f:
-            shutil.copyfileobj(req.raw, f)
-
-    @classmethod
-    def tearDownClass(cls):
-        shutil.rmtree(cls.avi_path, ignore_errors=True)
+        cls.avi_path = 'VID00003-20100701-2204.avi'
 
     def test_load_avi(self):
         cap = cv2.VideoCapture(self.avi_path)
