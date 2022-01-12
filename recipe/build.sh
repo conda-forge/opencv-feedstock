044d15063a589676681e41f36c75c67e017781fd
Mark Harfouche <mark.harfouche@gmail.com>
2022-01-12 14:40:01 +0530

    HAVE_TESSERACT=0

--- recipe/build.sh
+++ recipe/build.sh
@@ -119,6 +119,7 @@ cmake ${CMAKE_ARGS} -LAH -G "Ninja"                                       \
     -DWITH_TENGINE=0                                                      \
     -DWITH_GSTREAMER=0                                                    \
     -DWITH_MATLAB=0                                                       \
+    -DWITH_TESSERACT=0                                                    \
     -DWITH_VA=0                                                           \
     -DWITH_VA_INTEL=0                                                     \
     -DWITH_VTK=0                                                          \
