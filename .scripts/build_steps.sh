ec18a5b36b7efb1bef9ac8114d735357acd22da6
Mark Harfouche <mark.harfouche@gmail.com>
2024-03-12 23:18:31 -0400

    MNT: Re-rendered with conda-build 24.1.2, conda-smithy 3.31.1, and conda-forge-pinning 2024.03.12.21.52.52

--- .scripts/build_steps.sh
+++ .scripts/build_steps.sh
@@ -48,7 +48,7 @@ source run_conda_forge_build_setup
 # "recipe/yum_requirements.txt" file. After updating that file,
 # run "conda smithy rerender" and this line will be updated
 # automatically.
-/usr/bin/sudo -n yum install -y mesa-libGL mesa-libEGL libglvnd-glx libglvnd-opengl
+/usr/bin/sudo -n yum install -y mesa-libGL mesa-libEGL libglvnd-egl libglvnd-glx libglvnd-opengl
 
 
 # make the build number clobber
